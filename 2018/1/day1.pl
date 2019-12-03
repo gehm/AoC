781e0781f992579a98fcc16d860c4d5131d87069
Ekkehard 'Ekki' Gehm <gehm@luzifer.doom.de>
2019-12-03 01:43:02 +0100

    Well it took 144 Loops to get the solution... There must be a nicer way..

--- 2018/1/day1.pl
+++ 2018/1/day1.pl
@@ -2,24 +2,34 @@
 
 use strict;
 use warnings;
+use List::MoreUtils qw{any}; 
 
-open(my $fh, "<", "input.txt") or die "Ooops! No input.txt";
-
-my $freq =0;
-
-while (my $freq_change = <$fh>) {
-	chomp $freq_change;
-	my $operator = substr $freq_change,0,1;
-	my $value = substr $freq_change, 1;
-	#print "$operator,$value \n";
-	if ($operator eq "+") {
-		$freq += $value;
-	} else {
-		$freq -= $value;
+my $freq = 0;
+my @frequencies = ( 0, );
+for (my $i = 0; $i <1000; $i++) {
+	print "Loop $i\n";
+	open(my $fh, "<", "input.txt") or die "Ooops! No input.txt";
+	while (my $freq_change = <$fh>) {
+		chomp $freq_change;
+		my $operator = substr $freq_change,0,1;
+		my $value = substr $freq_change, 1;
+		#print "$operator,$value \n";
+		if ($operator eq "+") {
+			$freq += $value;
+		} else {
+			$freq -= $value;
+		}
+		
+		if ( any {$_ == $freq} @frequencies ) {
+			print "Duplicate: $freq";
+			exit;
+		} else {
+		#print "$freq\n";
+		 push @frequencies,$freq; 
+		}
 	}
+	close ($fh);
 }
 
-print " $freq \n";
 
-close ($fh);
 
