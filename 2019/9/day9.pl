23441b6decd7f3e0d700312a574b24b6bdfda4d2
Capt. Ekki <38411139+gehm@users.noreply.github.com>
2019-12-13 13:58:24 +0100

    Initial day9

---
+++ 2019/9/day9.pl
@@ -0,0 +1,24 @@
+﻿#!/usr/local/bin/perl
+
+
+use strict;
+use warnings;
+use threads;
+use threads::shared;
+use Thread::Queue;
+
+use List::Permutor;
+use List::Util qw( max );
+
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+use IntComp;
+
+my $input = "$FindBin::Bin/input.txt";
+open(my $fh, "<", $input ) or die "Ooops! No $input";
+my @int_prog = split(',', <$fh>);
+close $fh;
+
+
+my $a_queue = Thread::Queue->new(0);
+my $amp_a = threads->create(\&IntComp::run, $a_queue , $realprog, $a_queue, "A");
