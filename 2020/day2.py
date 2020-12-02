db3510df60be0d3070e49abb2c11cfc0a59155c6
Capt. Ekki <38411139+gehm@users.noreply.github.com>
2020-12-02 12:30:07 +0100

    Day2

---
+++ 2020/day2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# AoC 2020 Day 2
+good_pwds = 0
+good_pwds2 = 0
+fh = open("input/day2.txt", "r")
+line = fh.readline()
+
+
+def checkpass(pol, pwd):
+    (number, string) = pol.split()
+    (low, high) = number.split('-')
+
+    if int(low) <= pwd.count(string) <= int(high):
+        return True
+
+
+def checkpass2(pol, pwd):
+    (number, string) = pol.split()
+    pwd = pwd.strip()
+    (first, sec) = number.split('-')
+    first = int(first) - 1
+    sec = int(sec) - 1
+    if (pwd[first] == string) ^ (pwd[sec] == string):
+        return True
+
+
+while line:
+    (policy, passwd) = line.split(':')
+    if checkpass(policy, passwd):
+        good_pwds = good_pwds + 1
+    if checkpass2(policy, passwd):
+        good_pwds2 = good_pwds2 + 1
+    line = fh.readline()
+fh.close()
+
+print('The inputfile has ', good_pwds, ' valid passwords according to Part 1.\n')
+print('The inputfile has ', good_pwds2, ' valid passwords according to Part 2.\n')
+
+
