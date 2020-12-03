854a6b0f3d1b88e41c67f08da14361551be87151
Capt. Ekki <38411139+gehm@users.noreply.github.com>
2020-12-03 22:15:37 +0100

    Output both answers and keep all subs

--- 2020/day3.py
+++ 2020/day3.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # AoC 2020 Day 3
 trees = 0
-answer = 1
 run = 0
 x_step = [1, 3, 5, 7, 1]
 y_step = [1, 1, 1, 1, 2]
-
+answers = []
 
 with open("input/day3.txt") as fh:
     local_map = fh.readlines()
@@ -28,12 +27,18 @@ while run < len(x_step):
             x_pos = x_pos - len(line)
             if line[x_pos] == '#':
                 trees = trees + 1
-    print("Trees: ", trees, "in run ", run)
+    #print("Trees: ", trees, "in run ", run)
     run = run + 1
-    answer = answer * trees
+    answers.append(trees)
 
+prod_answer = 1
+for i in answers:
+    prod_answer = prod_answer * i
+    
+print("Day 3 of AoC 2020")
+print("Answer Part 1: ", answers[1])
+print("Answer Part 2: ", prod_answer)
 
-print("Answer: ", answer)
 
 
 
