466cfb4506292e80936bc2156e014b9893fa7ac1
Capt. Ekki <38411139+gehm@users.noreply.github.com>
2020-12-07 14:07:47 +0100

    Day 4 Part 2

--- 2020/day4.py
+++ 2020/day4.py
@@ -25,6 +25,31 @@ class ID:
     def print(self):
         print(self.byr, self.iyr, self.eyr, self.hgt, self.hcl, self.ecl, self.pid, self.cid)
 
+import re
+def validate(key,value):
+    if key == "byr" and int(value) in range(1920, 2003):
+        return True
+    elif key == "iyr" and int(value) in range(2010, 2021):
+        return True
+    elif key == "eyr" and int(value) in range(2020, 2031):
+        return True
+    elif key == "hgt":
+        res = re.findall(r'^(\d+)(in|cm)$', value)
+        for tuple in res:
+            if tuple[1] == "cm" and int(tuple[0]) in range(150, 194):
+                return True
+            elif tuple[1] == "in" and int(tuple[0]) in range(59, 77):
+                return True
+    elif key == "hcl" and re.search(r'^\#[a-f0-9]{6}$', value):
+        return True
+    elif key ==  "ecl" and value in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
+        return True
+    elif key == "pid" and re.search(r'^[0-9]{9}$', value):
+        return True
+    elif key == "cid":
+        return True
+    else:
+        return False
 
 with open("input/day4.txt") as fh:
     infile = fh.read().splitlines()
@@ -40,7 +65,8 @@ for line in infile:
     else:
         for pair in line.split(' '):
             (field, value) = pair.split(':')
-            setattr(id, field, value)
+            if validate(field, value):
+                setattr(id, field, value)
 id_batch.append(id)
 
 valid_ids = 0
